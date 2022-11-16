f0bb105e3107d10d921670504991fbfcc66e7f1f
yixin0289 <yixin0829@outlook.com>
2022-11-15 23:19:47 -0500

    Switch database setting to Postgres Heroku

--- project/app.py
+++ project/app.py
@@ -5,6 +5,7 @@ from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
+import os
 
 
 basedir = Path(__file__).resolve().parent
@@ -14,7 +15,10 @@ DATABASE = "flaskr.db"
 USERNAME = "admin"
 PASSWORD = "admin"
 SECRET_KEY = "change_me"
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
+url = os.getenv('DATABASE_URL', f'sqlite:///{Path(basedir).joinpath(DATABASE)}')
+if url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
+SQLALCHEMY_DATABASE_URI = url
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
